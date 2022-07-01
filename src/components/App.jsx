e33cd86c38817e667b93008090d0bcf715506c23
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-07-01 21:09:31 +0600

    item and completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import Calendar from "../pages/Calendar";
-import CreateTask from "../pages/CreateTask";
-import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
-import Footer from "./Shared/Footer";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Shared/Navbar";
+import Footer from "./Shared/Footer";
+import Home from "../pages/Home";
+import CreateTask from "../pages/CreateTask";
+import CompletedTask from "../pages/CompletedTask";
+import Calendar from "../pages/Calendar";
+import NotFound from "../pages/NotFound";
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateTask />} />
+        <Route path="/completed" element={<CompletedTask />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
