b06f05bbafe4f73e6299ae71e976ecd3cd6a82b0
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-07-01 20:33:49 +0600

    navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Shared/Navbar";
 import Footer from "./Shared/Footer";
 import Home from "../pages/Home";
-import CreateTask from "../pages/CreateTask";
-import CompletedTask from "../pages/CompletedTask";
-import Calendar from "../pages/Calendar";
 import NotFound from "../pages/NotFound";
 
 const App = () => {
@@ -14,9 +11,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateTask />} />
-        <Route path="/completed" element={<CompletedTask />} />
-        <Route path="/calendar" element={<Calendar />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
